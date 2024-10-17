a096931b1b7bf38aa43275904e5cf6a4889b52e8
Scott Bentham <Scott.Bentham@digital.homeoffice.gov.uk>
2024-10-17 15:16:31 +0100

    feat(*): show backend call notification

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux';
-import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect, useRef} from 'react';
+import Notification from './components/UI/Notification';
+import {uiSettingsActions} from './store/uiSettings';
 
 const DB_URL = 'https://example-project-e28b4-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
 
 function App() {
 
     const shouldDisplayCart = useSelector(state => state.uiSettings.displayCart);
+    const notification = useSelector(state => state.uiSettings.notification);
     const cart = useSelector(state => state.cart);
+    const dispatch = useDispatch();
+    const isInitial = useRef(true);
 
     useEffect( () => {
+        if (isInitial.current) {
+            isInitial.current = false;
+            return;
+        }
+
+        dispatch(uiSettingsActions.setNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+
         (async () => {
-            await fetch(DB_URL,
-                {
-                    method: 'PUT',
-                    body: JSON.stringify(cart),
-                });
+            try {
+                const response = await fetch(DB_URL,
+                    {
+                        method: 'PUT',
+                        body: JSON.stringify(cart),
+                    });
+                if (!response.ok) {
+                    throw new Error('Sending cart data failed.');
+                }
+                await response.json();
+                dispatch(uiSettingsActions.setNotification({
+                    status: 'success',
+                    title: 'Success!',
+                    message: 'Sent cart data successfully!'
+                }))
+            }
+            catch (error) {
+                dispatch(uiSettingsActions.setNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                }));
+            }
         })();
     }, [cart]);
 
   return (
-    <Layout>
-        { shouldDisplayCart && <Cart /> }
-      <Products />
-    </Layout>
+      <>
+        {notification && <Notification {...notification} />}
+        <Layout>
+            { shouldDisplayCart && <Cart /> }
+          <Products />
+        </Layout>
+      </>
   );
 }
 
--- src/store/uiSettings.js
+++ src/store/uiSettings.js
@@ -2,10 +2,13 @@ import {createSlice} from '@reduxjs/toolkit';
 
 const uiSettingsSlice = createSlice({
     name: 'cart',
-    initialState: {displayCart: false},
+    initialState: {displayCart: false, notification: null},
     reducers: {
         toggleCartDisplay(state) {
             state.displayCart = !state.displayCart;
+        },
+        setNotification(state, action) {
+            state.notification = action.payload;
         },
     }
 });
