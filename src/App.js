4f3de2e9d55a52bb64102400864f33607b1baa2f
Scott Bentham <Scott.Bentham@digital.homeoffice.gov.uk>
2024-10-17 14:25:53 +0100

    feat(*): send cart to firebase backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {Provider, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
+import {useEffect} from 'react';
+
+const DB_URL = 'https://example-project-e28b4-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
 
 function App() {
 
     const shouldDisplayCart = useSelector(state => state.uiSettings.displayCart);
+    const cart = useSelector(state => state.cart);
+
+    useEffect( () => {
+        (async () => {
+            await fetch(DB_URL,
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                });
+        })();
+    }, [cart]);
 
   return (
     <Layout>
